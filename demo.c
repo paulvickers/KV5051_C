ee15ebe42d5ee54b52b6f41b2714dbc21b85aa7b
Paul Vickers <paul.vickers@northumbria.ac.uk>
2024-09-20 17:03:25 +0100

    Formatted numbers

--- demo.c
+++ demo.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <math.h>
+
+float bookPrice = 9.99;
+int intBookPrice;
 
 int main () {
+	intBookPrice = bookPrice * 100 ;
+
 	printf ("Hello, World!\n");
+	printf ("Price of book is %2d.%2d\n", intBookPrice / 100, intBookPrice % 100);
+
+	bookPrice = 15.99;
+
+	intBookPrice = bookPrice * 100 ;
+
+	printf ("Price of book is %2d.%2d\n", intBookPrice / 100, intBookPrice % 100);
+
 
 	return 0;
 }
